0d9b1fbe0aa1145b0f4e4f61b084dd772f3f641a
Joe Jobes <jmrjobes@gmail.com>
2018-03-23 15:43:47 -0600

    Add integration test

---
+++ spec/integration/user_can_login_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature "user can log into spotify" do
+  context "from the homepage" do
+    scenario "user logs in" do
+      visit root_path
+
+      click_on "Sign into Spotify"
+
+      expect(page).to have_content("Welcome Joe")
+    end
+  end
+end
