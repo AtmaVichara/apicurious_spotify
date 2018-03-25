69cf932a09c1d193e8de83a8b9c80f654aeb35ae
Joe Jobes <jmrjobes@gmail.com>
2018-03-25 08:28:45 -0600

    Add update or create method and rename find_or_create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ApplicationRecord
 
-  def self.find_or_create(attributes)
+  def self.update_or_create(attributes)
     user = User.find_by(refresh_token: attributes[:refresh_token]) || User.new
     user.attributes = {
       oauth_token: attributes[:access_token],
       refresh_token: attributes[:refresh_token],
       expires_in: attributes[:expires_in]
     }
+    user.save!
+    user
   end
 
 end
