90374d7834a5394f077a23e2824cf510340d1cea
Joe Jobes <jmrjobes@gmail.com>
2018-03-25 08:23:35 -0600

    Add find_or_create method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
 
-  
+  def self.find_or_create(attributes)
+    user = User.find_by(refresh_token: attributes[:refresh_token]) || User.new
+    user.attributes = {
+      oauth_token: attributes[:access_token],
+      refresh_token: attributes[:refresh_token],
+      expires_in: attributes[:expires_in]
+    }
+  end
 
 end
