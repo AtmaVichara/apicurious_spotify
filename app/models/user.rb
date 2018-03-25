a7e97cc272b8fd7d38d19eae51f74a7d77227956
Joe Jobes <jmrjobes@gmail.com>
2018-03-25 11:49:39 -0600

    Fix user class method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,19 @@
 class User < ApplicationRecord
 
-  def self.update_or_create(attributes)
-    user = User.find_by(refresh_token: attributes[:refresh_token]) || User.new
+  def self.update_or_create(oauth_info)
+    user = User.find_by(name: oauth_info[:info][:name]) || User.new
     user.attributes = {
-      oauth_token: attributes[:access_token],
-      refresh_token: attributes[:refresh_token],
-      expires_in: attributes[:expires_in]
+      oauth_token: oauth_info[:credentials][:token],
+      refresh_token: oauth_info[:credentials][:refresh_token],
+      expires_in: (Time.now + 3600.seconds),
+      name: oauth_info[:info][:name]
     }
     user.save!
     user
+  end
+
+  def expired?
+    Time.now == expires_in
   end
 
 end
