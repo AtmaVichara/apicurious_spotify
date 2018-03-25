dd227f76dd6d2d2bde2d86a201d9b80bb5b89d8f
Joe Jobes <jmrjobes@gmail.com>
2018-03-25 11:48:39 -0600

    Add update_or_create method and delete extraneous objects

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,17 @@
 class SessionsController < ApplicationController
 
   def create
-
-    grant = Base64.strict_encode64("#{ENV["SPOTIFY_CLIENT"]}:#{ENV["SPOTIFY_SECRET"]}")
-
-    response = Faraday.post do |req|
-      req.url("https://accounts.spotify.com/api/token")
-      req.headers['Authorization'] = "Basic #{grant}"
-      req.params['grant_type'] = 'authorization_code'
-      req.params['code'] = params[:code]
-      req.params['redirect_uri'] = "http://localhost:3000/auth/spotify"
+    if user = User.update_or_create(request.env["omniauth.auth"])
+      session[:user_id]  = user.id
+      redirect_to dashboard_index_path
+    else
+      redirect_to root_path
     end
+  end
 
-    parsed_response = JSON.parse(response.body)
-
-    user = User.update_or_create(parsed_response)
-
-    redirect dashboard_index_path
+  def destroy
+    session.destroy
+    redirect_to root_path
   end
 
 end
