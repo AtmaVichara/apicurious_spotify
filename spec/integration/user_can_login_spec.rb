e8862249152d8d0d87d80f19ee7409a1fbbc4ee5
Joe Jobes <jmrjobes@gmail.com>
2018-03-25 11:47:48 -0600

    Add OmniauthHelper and add more to integration test

--- spec/integration/user_can_login_spec.rb
+++ spec/integration/user_can_login_spec.rb
@@ -3,11 +3,19 @@ require 'rails_helper'
 RSpec.feature "user can log into spotify" do
   context "from the homepage" do
     scenario "user logs in" do
+      stub_omniauth
       visit root_path
 
       click_on "Sign into Spotify"
+      user = User.first
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-      expect(page).to have_content("You have successfully logged in.")
+      expect(page).to have_content("You have successfully logged in jmrj222")
+      expect(page).to have_link("Logout")
+
+      click_on "Logout"
+
+      expect(page).to have_link("Sign into Spotify")
     end
   end
 end
